f484184dd860d0fc9bfb6ab21b874cb66fa56059
landpad <20844115+Landpad@users.noreply.github.com>
2024-02-10 22:21:35 -0300

    Now you can draw by simply clicking and dragging in the screen, added functionality to the color pallette and added an hex value input, added animation to the shake button and other minor improvements

--- script.js
+++ script.js
@@ -16,18 +16,18 @@ function createGrid (squaresPerSide){
 
 }
 
-function updateGridSquare(gridSquareId){
+function updateGridSquare(gridSquareId, color){
     const gridSquare = document.querySelector(`#gridSquare${gridSquareId}`);
-    gridSquare.style.background = 'red';
+    gridSquare.style.background = color;
 
 }
 
-function getGridSquare(event, squaresPerSide){
+function getGridSquare(event, squaresPerSide, color){
     let totalGridSquare = (squaresPerSide * squaresPerSide) - 1;
     for( let i = totalGridSquare; i>=0; i-- ){
         switch(event.target.id){
             case `gridSquare${i}`:
-                updateGridSquare(i);
+                updateGridSquare(i, color);
                 break;
         }
 
@@ -43,6 +43,17 @@ function removeGrid(){
 
 }
 
+function getSquaresPerSide (){
+    let squaresPerSide = prompt('Enter a number of squares per side less than 100.');
+    while (isNaN(parseInt(squaresPerSide)) || parseInt(squaresPerSide) > 100) {
+        alert('Please enter a valid number less than 100.');
+        squaresPerSide = prompt('Enter a number of squares per side less than 100.');
+    }
+
+    return squaresPerSide;
+
+}
+
 function resizeGrid(squaresPerSide){
     let totalGridSquare = (squaresPerSide * squaresPerSide) - 1;
     let gridSquareSize = 100 / squaresPerSide;
@@ -51,31 +62,80 @@ function resizeGrid(squaresPerSide){
         let gridSquare = document.querySelector(`#gridSquare${i}`);
         gridSquare.style.width = `${gridSquareSize}%`;
         gridSquare.style.height = `${gridSquareSize}%`;
+
     }
+
 }
+
+function playAudio(audioSrc){
+    let audio = new Audio(audioSrc);
+    audio.play();
+}
+
+function insertCharacter(element, character, position, endPosition){
+    if (!(element.value[0] === `${character}`)) {
+        element.setSelectionRange(position, endPosition);
+        element.value = `${character}` + element.value;
+
+    }
+
+    return element.value;
+
+}
+
+const DEFAULT_COLOR = '#000000';
+const ERASER_COLOR = '#ffffff'
+const DEFAULT_SQUARES_PER_SIDE = '16';
+const SHAKE_SOUND = './resources/Zumbido_Messenger.mp3';
 
 const gridContainer = document.querySelector('.gridContainer');
 const resetGridButton = document.querySelector('#resetGrid');
+const penColor = document.querySelector('#changeColor');
+const hexColor = document.querySelector('#hexColor');
+const eraser = document.querySelector('#eraser');
+
+let squaresPerSide = DEFAULT_SQUARES_PER_SIDE;
+let color = penColor.value;
+color = DEFAULT_COLOR;
+
+penColor.addEventListener('input', () =>{
+    color = penColor.value;
+    hexColor.value = color;
+});
+
+hexColor.addEventListener('input', () => {
+    insertCharacter(hexColor, '#', 0, 0);
+    penColor.value = hexColor.value;
+    color = penColor.value;
+});
+
+eraser.addEventListener('click', () =>{
+    color = ERASER_COLOR;
+});
 
 gridContainer.addEventListener('mouseover', (event) => {
-    getGridSquare(event, '100');
-
+    if(event.buttons == 1 || event.buttons == 4){
+        getGridSquare(event, squaresPerSide, color);
+    }
 });
 
-resetGridButton.addEventListener('click', () => {
-    let squaresPerSide = prompt('Enter a number of squares per side less than 100.');
-    
+gridContainer.addEventListener('click', (event) => {
+    getGridSquare(event, squaresPerSide, color);
+});
+
+resetGridButton.addEventListener('click', (event) => {
     removeGrid();
-
-    while (isNaN(parseInt(squaresPerSide)) || parseInt(squaresPerSide) > 100) {
-        alert('Please enter a valid number less than 100.');
-        squaresPerSide = prompt('Enter a number of squares per side less than 100.');
-    }
-
+    squaresPerSide = getSquaresPerSide();
     createGrid(squaresPerSide);
     resizeGrid(squaresPerSide);
-
-
+    event.preventDefault();
+    gridContainer.classList.add('gridContainerShake');
+    playAudio(SHAKE_SOUND);
+    
 });
 
-createGrid('16');
+gridContainer.addEventListener('animationend', () => {
+    gridContainer.classList.remove('gridContainerShake');
+});
+
+createGrid(DEFAULT_SQUARES_PER_SIDE);
